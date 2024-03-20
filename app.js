da9f77cf796cef247f540ada4376030b4a5cb866
el634dev <84924260+el634dev@users.noreply.github.com>
2024-03-20 18:17:30 -0400

    Completed first route

---
+++ app.js
@@ -0,0 +1,22 @@
+// Require Libraries
+const express = require('express');
+
+// ---------------
+// App Setup
+const app = express();
+
+// ---------------
+// Middleware
+
+// ---------------
+// Routes
+app.get('/', (req, res) => {
+    res.send('Hello Squirrel');
+});
+
+// ---------------
+// Start Server
+app.listen(3000, () => {
+    // See message in terminal
+    console.log('Gif Search listening on port localhost:3000!');
+});
